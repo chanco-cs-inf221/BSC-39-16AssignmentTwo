42c1e07dc830fd37b9a3610b5194a570a04d4edb
chrispharaoh <48133104+chrispharaoh@users.noreply.github.com>
2019-03-22 17:35:47 +0100

    Add files via upload

---
+++ processor.js
@@ -0,0 +1,11 @@
+ 
+
+
+var hello = () =>{
+  if(typeof(Storage) == "undefined") {
+        document.write('please use another broser or udate your browser to a new vesion');
+      }
+
+   }
+  
+  
